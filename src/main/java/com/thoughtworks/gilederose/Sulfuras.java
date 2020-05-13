1c3db15af5fb81bd637b051903f2c6f60feafa7a
SaiSea <saisea@saisea.com>
2020-05-13 23:44:46 +0800

    add unit tests for sulfuras :v: @bo.wang

---
+++ src/main/java/com/thoughtworks/gilederose/Sulfuras.java
@@ -0,0 +1,11 @@
+package com.thoughtworks.gilederose;
+
+public class Sulfuras extends Commodity {
+
+  public Sulfuras(int quality) {
+    super(quality);
+  }
+
+  protected void updatedQualityBy(int passedDays) {
+  }
+}
--- test/com/thoughtworks/gilederose/CommodityTest.java
+++ test/com/thoughtworks/gilederose/CommodityTest.java
@@ -39,4 +39,36 @@ public class CommodityTest {
     Assert.assertEquals(50, agedBrie.getQuality());
   }
 
+  @Test
+  public void should_return_0_when_passed_10_days_given_sulfuras_quality_is_0() {
+    Sulfuras sulfuras = new Sulfuras(0);
+    sulfuras.updatedQualityBy(10);
+
+    Assert.assertEquals(0, sulfuras.getQuality());
+  }
+
+  @Test
+  public void should_return_0_when_passed_20_days_given_sulfuras_quality_is_0() {
+    Sulfuras sulfuras = new Sulfuras(0);
+    sulfuras.updatedQualityBy(20);
+
+    Assert.assertEquals(0, sulfuras.getQuality());
+  }
+
+  @Test
+  public void should_return_50_when_passed_10_days_given_sulfuras_quality_is_50() {
+    Sulfuras sulfuras = new Sulfuras(50);
+    sulfuras.updatedQualityBy(10);
+
+    Assert.assertEquals(50, sulfuras.getQuality());
+  }
+
+
+  @Test
+  public void should_return_50_when_passed_20_days_given_sulfuras_quality_is_50() {
+    Sulfuras sulfuras = new Sulfuras(50);
+    sulfuras.updatedQualityBy(20);
+
+    Assert.assertEquals(50, sulfuras.getQuality());
+  }
 }
